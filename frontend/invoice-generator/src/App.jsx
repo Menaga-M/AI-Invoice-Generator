0c83b1e94fb9f046bdb138ac1c8d90ddded86ab3
Menaga-M <menaga7221@gmail.com>
2026-07-13 15:29:00 +0530

    Push : Invoice app project 2

--- frontend/invoice-generator/src/App.jsx
+++ frontend/invoice-generator/src/App.jsx
@@ -9,6 +9,12 @@ import {Toaster} from "react-hot-toast";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUp from "./pages/Auth/SignUp";
 import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import AllInvoices from "./pages/Invoices/AllInvoices";
+import CreateInvoice from "./pages/Invoices/CreateInvoice";
+import InvoiceDetail from "./pages/Invoices/InvoiceDetail";
+import Profile from "./pages/Profile/ProfilePage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 const App =  () => {
   return (
@@ -19,6 +25,14 @@ const App =  () => {
           <Route path="/" element={<LandingPage/>} />
           <Route path="/Signup" element={<SignUp/>} />
           <Route path="/Login" element={<Login/>} />
+
+          <Route path="/" element={<ProtectedRoute/>}>
+            <Route path="dashboard" element={<Dashboard/>} />
+            <Route path="invoices" element={<AllInvoices/>} />
+            <Route path="invoices/new" element={<CreateInvoice/>} />
+            <Route path="invoices/:id" element={<InvoiceDetail/>} />
+            <Route path="profile" element={<Profile/>} />
+          </Route>
 
           {/* Catch all routes */}
           <Route path="*any" element={<Navigate to="/" replace/>}/>
---
+++ frontend/invoice-generator/src/components/layout/DashboardLayout.jsx
@@ -0,0 +1,11 @@
+//import React from "react";
+
+const DashboardLayout =  ({children}) => {
+  return (
+    <div>Dashboard Layout
+        <div>{children}</div>
+    </div>
+  )
+};
+
+export default DashboardLayout;
