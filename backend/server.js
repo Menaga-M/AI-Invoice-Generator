435893427cded138508b8715c97cdfbee8b2d0cb
Menaga-M <menaga7221@gmail.com>
2026-07-17 20:42:21 +0530

    Push : connecting DB

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+const port = 3000
+
+app.use(
+    cors({
+        origin : "*",
+        method : ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders : ["Content-Type", "Authorization"],
+    })
+);
+
+connectDB();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
