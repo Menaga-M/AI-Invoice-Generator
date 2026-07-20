9347d93f34c2f08ee2ce82d9f42d1aa3ff942864
Menaga-M <menaga7221@gmail.com>
2026-07-20 12:20:10 +0530

    Push : Authentication update

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 const port = 3000
 
@@ -21,6 +23,8 @@ app.use(
 connectDB();
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
